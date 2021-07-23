d3985bf40c63b20b69790a4b846cbab7ede77fbb
Dawid Danielewicz <dawdan1x@gmail.com>
2021-07-23 10:45:18 +0200

    cors update

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: 'http://localhost:8080',
+  origin: 'http://localhost:8100',
 
   /*
   |--------------------------------------------------------------------------
